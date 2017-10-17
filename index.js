72c60aab18c46d5ed57f11094715974f25acebbb
Alastair Beaumont <a.beaumont11@gmail.com>
2017-10-16 17:21:23 -0700

    Updated the registerComponent to use main

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import { AppRegistry } from 'react-native';
 import App from './app/containers/app.js';
 import 'expo';
 
-AppRegistry.registerComponent('carorama', () => App);
+AppRegistry.registerComponent('main', () => App);
